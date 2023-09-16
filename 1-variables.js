0952d454f5d38bcd63ce72ac8e34aca335738fa9
DiveshR <divesh20july@gmail.com>
2023-09-16 05:40:31 +0530

    Variables in Javascript

---
+++ 1-variables.js
@@ -0,0 +1,7 @@
+const language = 'JavaScript';
+let library = 'React';
+var framework = 'Angular Js';
+backend = 'Node Js';
+let noValue;
+
+console.table([language, library,framework,backend,noValue]);
