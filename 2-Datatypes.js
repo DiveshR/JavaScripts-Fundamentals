283651187e5d09dd745de42c97840067c15aba0e
DiveshR <divesh20july@gmail.com>
2023-09-16 20:13:24 +0530

    Datatypes in javascript

---
+++ 2-Datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; //act all code as newer version
+
+// Primitive Datatypes
+// number => 1, 
+// bigint,
+// string => ""
+// boolean => true/false
+// null => standalone Value
+// undefined
+// Symbol => unique
+
+//Non Primitive Data Types
+// Object
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
