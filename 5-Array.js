627a77a466980cdeb0f8ba2c32ccb3209c308413
DiveshR <divesh20july@gmail.com>
2023-09-22 08:29:37 +0530

    More Array Methods are added

--- 5-Array.js
+++ 5-Array.js
@@ -40,10 +40,51 @@ console.log(typeof convertedString) //string
 //slice
 const originalArray = [100,200,300,400,500]
 console.log(originalArray.slice(1,4)) //[ 200, 300, 400 ]
-console.log(originalArray)
+console.log(originalArray)  //[100,200,300,400,500]
 
 // splice
 
 console.log(originalArray.splice(1,4)) //[ 200, 300, 400, 500 ]
 console.log(originalArray) // [100]
 
+const countries = ['India','US','Russia','UK','Germany']
+const capital = ['New Delhi','Washington','Moscow','London','Berlin']
+const currency  = ['Rupees','Dollar','Ruble','Pound','Euro']
+
+const pushArray = countries.push(capital) 
+
+console.log(pushArray) // ['India','US','Russia','UK','Germany',[ 'New Delhi', 'Washington', 'Moscow', 'London', 'Berlin' ]]
+
+const concatArray = countries.concat(capital)  
+console.log(concatArray) //['India','US','Russia','UK','Germany', 'New Delhi', 'Washington', 'Moscow', 'London', 'Berlin' ]
+
+// Alternate of concat is spread operator
+const spreadArray = [...countries,...capital,...currency]
+console.log(spreadArray)  //['India','US','Russia','UK','Germany', 'New Delhi', 'Washington', 'Moscow', 'London', 'Berlin','Rupees','Dollar','Ruble','Pound','Euro' ]
+
+// flat Array
+const monthsArray = ['Jan','Feb',['Mar'],['Apr','May','June'],'July','Aug',['Sep','Oct'],['Nov'],'Dec']
+// console.log(monthsArray)
+const  inFlatArray = monthsArray.flat(Infinity)
+console.log(inFlatArray)
+
+const  isArrayMethod = Array.isArray([1,2,3]) 
+console.log(isArrayMethod) //true
+
+const  isArrayMethodCheck = Array.isArray("Javascrit") 
+console.log(isArrayMethodCheck) //false
+
+const fromArray = Array.from('Javascript')
+console.log(fromArray) // ['J', 'a', 'v', 'a','s', 'c', 'r', 'i','p', 't']
+
+console.log(Array.from({'language': 'Javascript'})) // []
+
+console.log(Object.values({'language': 'Javascript'})) // Javascript
+
+
+const language1 = 'Python'
+const language2 = "Ruby & Rails"
+const language3 = "JavaScript"
+const language4 = "C++"
+
+console.log(Array.of(language1,language2,language3,language4)) // [ 'Python', 'Ruby & Rails', 'JavaScript', 'C++' ]
